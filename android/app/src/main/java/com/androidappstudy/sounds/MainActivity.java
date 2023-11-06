5e064f35a775b5603b506e7ae5019c59fae91835
jby585748 <jjboyeong@gmail.com>
2023-11-06 19:44:45 +0900

    Feat #1 안드로이드앱 Sounds 페이지 UI 1차 작업본

---
+++ android/app/src/main/java/com/androidappstudy/sounds/MainActivity.java
@@ -0,0 +1,18 @@
+package com.androidappstudy.sounds;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+    }
+}
